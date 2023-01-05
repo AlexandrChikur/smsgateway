75b608674e35f48c05717585b850460b8119dce6
Alexandr Chikur <alexandr.chikur@mail.ru>
2023-01-05 14:25:04 +0300

    Remove migrations and debug lines

--- backend/core/consumers.py
+++ backend/core/consumers.py
@@ -26,43 +26,8 @@ class MessageConsumer(mixins.PatchModelMixin,
 
     @model_observer(Message)
     async def message_activity(self, message, observer=None, **kwargs):
-        print(kwargs)
-        print(f"message_activity: {message}")
         await self.send_json(message)
 
     @message_activity.serializer
     def message_activity(self, instance: Message, action, **kwargs):
-        print(kwargs)
-        # messages = Message.objects.filter(author_id=self.scope['user'].id).order_by('-created_at')[:self.scope['limit']]
         return dict(data=MessageSerializer(instance).data, action=action.value, pk=instance.pk)
-
-    # @action()
-    # async def subscribe(self, **kwargs):
-    #     await self.message_activity.subscribe(author_id=self.scope['user'].id)
-
-    # def get_queryset(self):
-    #     return Message.objects.all()  # filter(author_id=self.request.user.id).order_by("-created_at")
-    #
-    # @action()
-    # async def join(self, pk, **kwargs):
-    #     print(self.scope)
-    #     print(kwargs)
-    #     print(pk)
-    #     for group in self.groups:
-    #         print(f"GROUP: {group}")
-    #         await self.channel_layer.group_send(
-    #             group,
-    #             {"test": 1}
-    #         )
-    #     await self.channel_layer.group_send()
-    #
-    # @model_observer(Message)
-    # async def message_activity(self, message, observer=None, **kwargs):
-    #     await self.send_json(message)
-    #
-    # @message_activity.serializer
-    # def message_activity(self, instance: Message, action, **kwargs):
-    #     return dict(data=MessageSerializer(instance).data, action=action.value, pk=instance.pk)
-
-    # @database_sync_to_async
-    # def
--- backend/core/middleware.py
+++ backend/core/middleware.py
@@ -17,26 +17,6 @@ from datetime import datetime
 ALGORITHM = "HS256"
 
 
-# @database_sync_to_async
-# def get_user(token):
-#     try:
-#         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=ALGORITHM)
-#         print(f"payload: {payload}")
-#     except Exception as exc:
-#         raise
-#         return AnonymousUser()
-#
-#     token_exp = datetime.fromtimestamp(payload['exp'])
-#     if token_exp < datetime.now():
-#         return AnonymousUser()
-#
-#     try:
-#         user = User.objects.get(id=payload['user_id'])
-#     except User.DoesNotExist:
-#         return AnonymousUser()
-#
-#     return user
-
 @database_sync_to_async
 def get_user(token_key):
     try:
@@ -46,43 +26,11 @@ def get_user(token_key):
         return AnonymousUser()
 
 
-# class TokenAuthMiddleware(BaseMiddleware):
-#     async def __call__(self, scope, receive, send):
-#         close_old_connections()
-#         print(scope)
-#         try:
-#             token_key = (dict((x.split('=') for x in scope['query_string'].decode().split("&")))).get('token')
-#             print(token_key)
-#         except ValueError:
-#             token_key = None
-#
-#         scope['user'] = await get_user(token_key)
-#         print(scope['user'])
-#         return await super().__call__(scope, receive, send)
-#
-#
-# def JwtAuthMiddlewareStack(inner):
-#     return TokenAuthMiddleware(inner)
-
 class TokenAuthMiddleware(BaseMiddleware):
     def __init__(self, inner):
         super().__init__(inner)
 
     async def __call__(self, scope, receive, send):
-        # print("============SCOPE============")
-        # for k,v in scope.items():
-        #     print('{0}:{1}'.format(k,v))
-        #
-        # print("============RECEIVE============")
-        # print(receive)
-        # print(type(receive))
-        # print(dir(receive))
-        #
-        # print("============SEND============")
-        # print(send)
-        # print(type(send))
-        # print(dir(send))
-
         close_old_connections()
         try:
             token_key = (dict((x.split('=') for x in scope['query_string'].decode().split("&")))).get('token', None)
